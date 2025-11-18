2a0332c91de9ed8a87f98bdda33a83f792e5e630
Matt <octopushorse@outlook.com>
2025-11-18 16:59:30 +0000

    add colour

--- networkGen/generateNetwork.py
+++ networkGen/generateNetwork.py
@@ -1,15 +1,20 @@
 import sys
+import json
 import time
 import linecache
 from collections import defaultdict
 
 execution_log = []
 last_time = None
+firstLine = None
 
 def trace_func(frame, event, arg):
     global last_time
+    global firstLine
 
     if event == "line":
+        if not firstLine:
+            firstLine = frame.f_lineno
         code = frame.f_code
         lineno = frame.f_lineno
         filename = code.co_filename
@@ -23,7 +28,7 @@ def trace_func(frame, event, arg):
 
         execution_log.append({
             "file": filename,
-            "line_no": lineno,
+            "line_no": lineno-firstLine,
             "code": line,
             "duration_since_last": duration,
         })
@@ -78,21 +83,63 @@ def calculateConnections(exec_log):
     return linePairCount
     print(linePairCount)
 
+import numpy as np
+def spreadOutPoints(execPercent, i):
+    arr = []
+    for k in execPercent:
+        arr.append(execPercent[k])
+
+    arr = np.array(arr)
+    arr_norm = (arr - arr.min()) / (arr.max() - arr.min())
+
+    return arr_norm[i]
+
+# def percentToRgb(p):
+#     res = {}
+#     res['r'] = 255 * p
+#     res['g'] = 255 * (1 - p)
+#     res['b'] = 0
+#     res['opacity'] = 1
+#     return res
+
 def percentToRgb(p):
-    res = {}
-    res['r'] = 255 * p
-    res['g'] = 255 * (1 - p)
-    res['b'] = 0
-    res['opacity'] = 1
-    return res
+    c1 = {}
+    c1['r'] = 247 
+    c1['g'] = 169
+    c1['b'] = 168
+
+    c2 = {}
+    c2['r'] = 60 
+    c2['g'] = 0 
+    c2['b'] = 0
+
+    return {
+        'r': int(c1['r'] * (1 - p) + c2['r'] * p),
+        'g': int(c1['g'] * (1 - p) + c2['g'] * p),
+        'b': int(c1['b'] * (1 - p) + c2['b'] * p),
+        'opacity': 1
+    }
+
+# def generateNodes(execPercent, execCount):
+#     res = []
+#     for lineNum in execPercent:
+#         currNode = {}
+#         currNode['id'] = str(lineNum)
+#         currNode['size'] = 1000 * execCount[int(lineNum)]
+#         currNode['color'] = percentToRgb(execPercent[lineNum])
+#         res.append(currNode)
+#     return res
 
 def generateNodes(execPercent, execCount):
     res = []
-    for lineNum in execPercent:
-        currNode = {}
-        currNode['id'] = str(lineNum)
-        currNode['size'] = 1000 * execCount[int(lineNum)]
-        currNode['color'] = percentToRgb(execPercent[lineNum])
+    for i, lineNum in enumerate(execPercent):
+        rgb = percentToRgb(spreadOutPoints(execPercent, i))
+        currNode = {
+            'id': str(lineNum),
+            'line': int(lineNum),   # IMPORTANT for highlighting
+            'size': 1000 * execCount[int(lineNum)],
+            'color': f"rgba({rgb['r']},{rgb['g']},{rgb['b']},{rgb['opacity']})"
+        }
         res.append(currNode)
     return res
 
@@ -107,6 +154,7 @@ def generateConnections(connections):
     return res
 
 
+
 def displayNodes(nodes):
     for node in nodes:
         print(node, ',')
@@ -115,6 +163,19 @@ def displayNodes(nodes):
 def displayConnections(connections):
     for link in connections:
         print(link, ',')
+
+def export_to_json(nodes, connections, filename="../d3/graph_data.json"):
+    displayNodes(nodes)
+    displayConnections(connections)
+    data = {
+        "nodes": nodes,
+        "links": connections
+    }
+
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=2)
+
+    print(f"[OK] Exported graph data to {filename}")
 
 # Example code to trace
 def main():
@@ -129,15 +190,12 @@ def main():
             return 0
         if (r,c) in seen:
             return 0
-
         seen.add((r,c))
-        
         up = dfs(r-1, c)
         down = dfs(r+1, c)
         left = dfs(r, c-1)
         right = dfs(r, c+1)
         return up + down + left + right + 1
-    
     res = 0
     for r in range(ROW):
         for c in range(COL):
@@ -154,8 +212,7 @@ if __name__ == "__main__":
 
     nodes = generateNodes(execPercent, execCount)
     connections = generateConnections(connectionDict)
-    displayNodes(nodes)
-    displayConnections(connections)
+    export_to_json(nodes, connections)
 
 
 
